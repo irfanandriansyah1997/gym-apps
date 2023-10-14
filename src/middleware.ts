7d251be710adc0c334f186e4a49c82f572434478
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2023-10-14 15:09:20 +0700

    fix(times): fix timezone

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
-import type { NextRequest} from "next/server";
+import type { NextRequest } from "next/server";
 import { NextResponse, userAgent } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { device } = userAgent(request);
   const viewport = device.type === "mobile" ? "mobile" : "desktop";
+
+  if (typeof window === "undefined") {
+    process.env.TZ = "Asia/Jakarta";
+  }
 
   request.nextUrl.searchParams.set("viewport", viewport);
   return NextResponse.rewrite(request.nextUrl);
